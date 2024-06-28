e6697b8a7f481590a68b1fe45c27e3b65b2a34db
abrahamparn <abrahamnaiborhu@gmail.com>
2024-06-28 08:51:31 +0700

    4.1 Blog List, step 1

---
+++ part3/backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const cors = require("cors");
+const notesRouter = require("./controllers/notes.controller");
+const middleware = require("./utils/middleware");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+app.use("/api/notes", notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
