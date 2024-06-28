2c654f5c803b5f41fb21ce1214ca4ce07e6bae64
abrahamparn <abrahamnaiborhu@gmail.com>
2024-06-28 11:15:33 +0700

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/BlogList/tests/blog.test.js
+++ part4/BlogList/tests/blog.test.js
@@ -13,7 +13,7 @@ describe("blog test", () => {
   });
 });
 
-describe("totla likes", () => {
+describe("total likes", () => {
   test("if empty list returns zero", () => {
     assert.strictEqual(listHelper.totalLikes([]), 0);
   });
@@ -24,5 +24,19 @@ describe("totla likes", () => {
 
   test("of a bigger list is calculated right", () => {
     assert.strictEqual(listHelper.totalLikes(blogTest), 36);
+  });
+});
+
+describe("Favorite Blog", () => {
+  const mostFavorite = {
+    __v: 0,
+    _id: "5a422b3a1b54a676234d17f9",
+    author: "Edsger W. Dijkstra",
+    likes: 12,
+    title: "Canonical string reduction",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+  };
+  test("finds which blog has the most likes", () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog(blogTest), mostFavorite);
   });
 });
--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -8,7 +8,21 @@ const totalLikes = (blogs) => {
   return totalLikes;
 };
 
+const favoriteBlog = (blogs) => {
+  let favoriteBlogName;
+  let counting = 0;
+
+  blogs.forEach((blog) => {
+    if (blog.likes > counting) {
+      counting = blog.likes;
+      favoriteBlogName = blog;
+    }
+  });
+  return favoriteBlogName;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
