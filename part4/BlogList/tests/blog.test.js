9ef2021c9abdd608f42fb54cb225b60bc35f0e42
abrahamparn <abrahamnaiborhu@gmail.com>
2024-06-28 11:07:11 +0700

    4.4: Helper Functions and Unit Tests, step 2

--- part4/BlogList/tests/blog.test.js
+++ part4/BlogList/tests/blog.test.js
@@ -1,6 +1,8 @@
 const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
+const blogModel = require("../models/blog.model");
+const blogTest = require("./blog.test.data");
 
 describe("blog test", () => {
   test("dummy returns one", () => {
@@ -8,5 +10,19 @@ describe("blog test", () => {
     const result = listHelper.dummy(blogs);
 
     assert.strictEqual(result, 1);
+  });
+});
+
+describe("totla likes", () => {
+  test("if empty list returns zero", () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0);
+  });
+  test("when list has one blogModel, equal the likes of that", () => {
+    const oneBlog = [blogTest[0]];
+    assert.strictEqual(listHelper.totalLikes(oneBlog), 7);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    assert.strictEqual(listHelper.totalLikes(blogTest), 36);
   });
 });
--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+  const totalLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  return totalLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
