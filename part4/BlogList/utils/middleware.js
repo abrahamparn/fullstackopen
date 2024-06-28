57aca36d772b963248e81a1c384fcbbddd7d4c9a
abrahamparn <abrahamnaiborhu@gmail.com>
2024-06-28 09:05:27 +0700

    4.2 Blog List, step 2

--- part4/BlogList/utils/middleware.js
+++ part4/BlogList/utils/middleware.js
@@ -19,8 +19,6 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return res.status(400).json({ error: error.message });
-  } else {
-    return res.status(400).json({ errr: error.message });
   }
 
   next(error);
