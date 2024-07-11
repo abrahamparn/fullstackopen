498d4b28c529dc2a57bccac01c86f45d4799b832
abrahamparn <abrahamnaiborhu@gmail.com>
2024-07-11 16:37:58 +0700

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
 
-export default Blog
+  const toggleVisibility = () => {
+    console.log("the Visibility: ", visible);
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={toggleVisibility}>View Detail</button>
+      {visible ? (
+        <>
+          <div>
+            {blog.url}
+            <br />
+            {blog.likes} <button>Like</button>
+            <br />
+            {blog.author}
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
