7a8d1406eebf6e38d1952cc6d8cdb44145724ed0
abrahamparn <abrahamnaiborhu@gmail.com>
2024-07-18 11:59:42 +0700

    6.10 Better Anecdotes, step 8

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,15 +1,8 @@
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import store from "../store";
 import App from "./App";
-import filterReducer from "./reducers/filterReducer";
-import anecdoteReducer from "./reducers/anecdoteReducer";
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
-});
-const store = createStore(reducer);
 store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,33 @@
-const filterReducer = (state = "ALL", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const initialState = "ALL";
+// const filterReducer = (state = "ALL", action) => {
+//   switch (action.type) {
+//     case "SET_FILTER":
+//       return action.payload;
+//     default:
+//       return state;
+//   }
+// };
+
+// export const filterChange = (filter) => {
+//   return {
+//     type: "SET_FILTER",
+//     payload: filter,
+//   };
+// };
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    filterChange(state, action) {
+      const filter = action.payload;
       return action.payload;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export const filterChange = (filter) => {
-  return {
-    type: "SET_FILTER",
-    payload: filter,
-  };
-};
+export const { filterChange } = filterSlice.actions;
 
-export default filterReducer;
+export default filterSlice.reducer;
