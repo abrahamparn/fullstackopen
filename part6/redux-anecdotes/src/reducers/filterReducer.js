8f10294713ce3cc1792de33826945f75283a37e6
abrahamparn <abrahamnaiborhu@gmail.com>
2024-07-19 10:14:09 +0700

    6.12 Better Anecdotes, step 10

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,21 +1,6 @@
 import { createSlice, current } from "@reduxjs/toolkit";
 
 const initialState = "ALL";
-// const filterReducer = (state = "ALL", action) => {
-//   switch (action.type) {
-//     case "SET_FILTER":
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// };
-
-// export const filterChange = (filter) => {
-//   return {
-//     type: "SET_FILTER",
-//     payload: filter,
-//   };
-// };
 
 const filterSlice = createSlice({
   name: "filter",
